007414c4241ac89689f0e1a2e4568c1d6222e65f
oroquest <pgutknecht1978@gmail.com>
2025-08-12 20:04:42 +0200

    Delete netlify/functions directory

--- netlify/functions/batch-token.js
+++
@@ -1,8 +0,0 @@
-export const handler = async () => {
-  // Hier koenntest du geplante Token fuer Folgeprozesse generieren
-  // und maskierte Events an Mailjet/Ops senden. Keine Voll-IBANs.
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ status:"ok" })
-  };
-};
